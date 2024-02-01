44405badf63d6ca964b5f0da1e7de8a5b88fd04c
Sonter <sonterkub@gmail.com>
2024-02-01 20:18:48 +0300

    Fix gun rotation

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -1,6 +1,14 @@
 extends Sprite2D
 
+var fun_allowed = false
+
 @onready var player: CharacterBody2D = $"../../Player"
+@onready var end_of_gun = $"Muzzle"
+
+var Bullet = preload("res://scenes/bullet.tscn")
+
+var aim_point = Vector2()
+const offset_of_player = 30
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,16 +17,30 @@ func _ready():
 
 
 func _process(delta):
-	#look_at(get_global_mouse_position())
+	var mouse = get_global_mouse_position()
+	if Input.is_action_just_pressed("shoot") and not fun_allowed or \
+		fun_allowed and Input.is_action_pressed("shoot"):
+		shoot()
 	
 	# FIXME: we should rotate around player and look at mouse
-	const offset_of_player = Vector2(50, 10)
-	global_transform = player.global_transform * Transform2D().translated(offset_of_player.rotated(get_angle_to(get_global_mouse_position())))
-	
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#
-	##rotate_object_local(Vector2(1, 0), 0.1)
-	##rotate_toward()
-	##position = $Player.position + (position - $Player.position)
-	#look_at(get_global_mouse_position())
+	#global_transform = player.global_transform * Transform2D().translated(offset_of_player.rotated(get_angle_to(get_global_mouse_position())))
+	#global_position = get_global_mouse_position()
+	if distance(mouse, player.global_position) <= offset_of_player:
+		global_position = mouse
+		aim_point = player.position
+	else:
+		global_position = player.global_position + (mouse - player.global_position).normalized() * offset_of_player
+		aim_point = mouse
+	look_at(aim_point)
+
+
+func distance(vec1, vec2):
+	vec1 -= vec2
+	return sqrt(vec1.x ** 2 + vec1.y ** 2)
+
+
+func shoot():
+	var bullet = Bullet.instantiate()
+	owner.add_child(bullet)
+	bullet.global_transform = end_of_gun.global_transform
+	bullet.look_at(aim_point)
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,6 @@
 extends CharacterBody2D
 
-
-var Bullet = preload("res://scenes/bullet.tscn")
 @export var speed = 250
-
-@onready var end_of_gun = $"Gun/Muzzle"
-
 
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,7 +8,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var movement_direction := Vector2.ZERO
-
+	
+	# FIXME: normalize this:
 	if Input.is_action_pressed("up"):
 		movement_direction.y = -1 
 	if Input.is_action_pressed("down"):
@@ -22,21 +18,10 @@ func _process(delta: float) -> void:
 		movement_direction.x = -1
 	if Input.is_action_pressed("right"):
 		movement_direction.x = 1
-	if Input.is_action_pressed("shoot"):
-		shoot()
+
 
 	movement_direction = movement_direction.normalized()
 	velocity = movement_direction * speed
 	move_and_slide()
 
 	#look_at(get_global_mouse_position())
-
-#func _unhandled_input(event: InputEvent) -> void:
-	
-
-
-func shoot():
-	var bullet = Bullet.instantiate()
-	owner.add_child(bullet)
-	bullet.transform = end_of_gun.global_transform
-	bullet.look_at(get_global_mouse_position())
