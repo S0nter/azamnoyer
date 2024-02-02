4b3e9a1c77573bbcdbed4950415ed94ab220d693
Sonter <sonterkub@gmail.com>
2024-02-02 20:41:12 +0300

    Minor fix

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -25,27 +25,25 @@ func _process(delta):
 	aim_vector = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down").normalized()  # for gamepad/joystick
 	
 	offset_gun()
-	
 	rotate_gun()
-	
 	handle_input()
-	
-	flip_if_needed()
 
 
 func offset_gun():
+	var player_pos = player.global_position
 	if fun_allowed:
+		# this is wrong way to do this stuff but it doesn't make it less fun
 		global_transform = player.global_transform * Transform2D().translated(Vector2(offset_of_player, offset_of_player).rotated(get_angle_to(mouse)))
 		## disable look_at(...) ##
 		aim_point = global_position 	# uncontrollable in all directions
 		#aim_point = position        	# swirl
 	elif aim_vector != Vector2(0, 0):
-		global_position = player.global_position + aim_vector * offset_of_player		
-	elif distance(mouse, player.global_position) <= offset_of_player:
+		global_position = player_pos + aim_vector * offset_of_player		
+	elif player_pos.distance_to(mouse) <= offset_of_player:
 		global_position = mouse
 		aim_point = player.position
 	else:
-		global_position = player.global_position + (mouse - player.global_position).normalized() * offset_of_player
+		global_position = player_pos + (mouse - player_pos).normalized() * offset_of_player
 
 
 func rotate_gun():
@@ -53,24 +51,18 @@ func rotate_gun():
 		look_at(aim_point)
 	else:
 		look_at(global_position + aim_vector)
-
-
-func handle_input():
-	if Input.is_action_just_pressed("shoot") and not fun_allowed or \
-		fun_allowed and Input.is_action_pressed("shoot"):
-		shoot()
-
-
-func flip_if_needed():
+	
+	# flip if needed
 	if player.global_position.x > global_position.x:
 		flip_v = true
 	else:
 		flip_v = false
 
 
-func distance(vec1, vec2):
-	vec1 -= vec2
-	return sqrt(vec1.x ** 2 + vec1.y ** 2)
+func handle_input():
+	if Input.is_action_just_pressed("shoot") and not fun_allowed or \
+		fun_allowed and Input.is_action_pressed("shoot"):
+		shoot()
 
 
 func shoot():
