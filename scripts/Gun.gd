c7669f817e1c3bf28628390d45c51d38d49f2d4f
Sonter <sonterkub@gmail.com>
2024-02-02 19:03:28 +0300

    Improve gamepad support

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -9,6 +9,7 @@ var fun_allowed = false
 var Bullet = preload("res://scenes/bullet.tscn")
 
 var aim_point = Vector2()
+var aim_vector = Vector2()
 const offset_of_player = 30
 
 
@@ -20,23 +21,31 @@ func _ready():
 func _process(delta):
 	var mouse = get_global_mouse_position()
 	aim_point = mouse
-	
-	if Input.is_action_just_pressed("shoot") and not fun_allowed or \
-		fun_allowed and Input.is_action_pressed("shoot"):
-		shoot()
+	aim_vector = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down").normalized()  # for gamepad/joystick
 	
 	if fun_allowed:
 		global_transform = player.global_transform * Transform2D().translated(Vector2(offset_of_player, offset_of_player).rotated(get_angle_to(mouse)))
 		## disable look_at(...) ##
 		aim_point = global_position 	# uncontrollable in all directions
 		#aim_point = position        	# swirl
+	elif aim_vector != Vector2(0, 0):
+		global_position = player.global_position + aim_vector * offset_of_player		
 	elif distance(mouse, player.global_position) <= offset_of_player:
 		global_position = mouse
 		aim_point = player.position
 	else:
 		global_position = player.global_position + (mouse - player.global_position).normalized() * offset_of_player
-	look_at(aim_point)
+	#look_at(aim_point)
 	
+	if aim_vector == Vector2(0, 0):
+		look_at(aim_point)
+	else:
+		look_at(global_position + aim_vector)
+	
+	if Input.is_action_just_pressed("shoot") and not fun_allowed or \
+		fun_allowed and Input.is_action_pressed("shoot"):
+		shoot()
+
 	if player.global_position.x > global_position.x:
 		flip_v = true
 	else:
@@ -52,4 +61,4 @@ func shoot():
 	var bullet = Bullet.instantiate()
 	get_tree().get_root().add_child(bullet)
 	bullet.global_transform = end_of_gun.global_transform
-	bullet.look_at(aim_point)
+	bullet.look_at(end_of_gun.global_position + aim_vector)
