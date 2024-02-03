68d3d74b432d96f15a1c8af4aef0513497ad27b3
Sonter <sonterkub@gmail.com>
2024-02-03 19:54:10 +0300

    Add virtual joysticks for mobile

--- assets/scripts/gun.gd
+++ assets/scripts/gun.gd
@@ -42,23 +42,27 @@ func offset_gun():
 		# this is wrong way to do this stuff but it doesn't make it less fun
 		global_transform = player.global_transform * Transform2D().translated(Vector2(offset_of_player, offset_of_player).rotated(get_angle_to(mouse)))
 		## disable look_at(...) ##
-		aim_point = global_position 	# uncontrollable in all directions
-		#aim_point = position        	# swirl
+		set_aim_point(global_position)  	# uncontrollable in all directions
+		#set_aim_point(position)         	# swirl
 	elif aim_vector != Vector2(0, 0):
-		global_position = player_pos + aim_vector * offset_of_player		
+		
+		global_position = player_pos + aim_vector * offset_of_player
 	elif player_pos.distance_to(mouse) <= offset_of_player:
+		
 		global_position = mouse
-		aim_point = player.position
-		aim_vector = global_position - aim_point
-	else:
+		set_aim_point(player.position)
+		#aim_vector = global_position - aim_point
+		#flip_h = true
+	elif not on_mobile():
+		print(2)
 		global_position = player_pos + (mouse - player_pos).normalized() * offset_of_player
 
 
 func rotate_gun():
-	if aim_vector == Vector2(0, 0):
-		look_at(aim_point)
-	else:
-		look_at(global_position + aim_vector)
+	#if aim_vector == Vector2(0, 0):
+	look_at(global_position + aim_vector)
+	#else:
+		#look_at(global_position + aim_vector)
 	
 	# flip if needed
 	flip_v = 90 < abs(int(rotation_degrees) % 360) and abs(int(rotation_degrees) % 360) < 270
@@ -69,8 +73,11 @@ func handle_input():
 	var delta = get_process_delta_time()
 	sec_since_shot += delta
 	
+	if sec_since_shot < cooldown:
+		return
 	
-	if Input.is_action_pressed("shoot") and sec_since_shot > cooldown:
+	if Input.is_action_pressed("shoot") and not on_mobile() or \
+	on_mobile() and aim_vector != Vector2(0, 0):
 		shoot()
 		sec_since_shot = 0
 
@@ -83,3 +90,14 @@ func shoot():
 	
 	if fun_allowed:
 		bullet.look_at(mouse)
+
+
+func set_aim_point(value: Vector2):
+	#last_aim_point = aim_point
+	aim_point = value
+	aim_vector = (global_position - aim_point).normalized()
+	print(aim_point, aim_vector)
+	
+
+func on_mobile() -> bool:
+	return DisplayServer.is_touchscreen_available()
